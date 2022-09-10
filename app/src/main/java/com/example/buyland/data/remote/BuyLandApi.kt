603ac99312c7d39562eb5a03c7fdb1ed7805cea1
Eradaviraf <99541476+ERa-aRE@users.noreply.github.com>
2022-09-10 18:21:45 +0430

    Update BuyLandApi.kt

--- app/src/main/java/com/example/buyland/data/remote/BuyLandApi.kt
+++ app/src/main/java/com/example/buyland/data/remote/BuyLandApi.kt
@@ -8,12 +8,12 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface BuyLandApi {
-    @GET("GetListTNazarat/PlatformV199325694,3")
+    @GET("GetListTNazarat/***********,3")
     suspend fun getComments():CommentDto
 
 //{Unique_code},{UsersId},{ServiserId},{TextNazar_},{NumRank},{OrderId}
 
-    @POST("AddNazarForServiser/PlatformV199325694,1,3,{TextNazar_},{NumRank},30098")
+    @POST("AddNazarForServiser/************,1,3,{TextNazar_},{NumRank},30098")
     fun postComment(
       @Path("TextNazar_")TextNazar_:String ,@Path("NumRank")NumRank :String
     ): Call<String?>
